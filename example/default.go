f9e1588f64e851fe4852c79fda4dee0068721e5c
othon <162609004+othon-hugo@users.noreply.github.com>
2024-07-22 13:17:24 -0300

    refactor(example): move features from 'default.go', 'marshal.go', and 'unmarshal.go' to 'jwt.go'

--- example/default.go
+++
@@ -1,8 +0,0 @@
-package example
-
-type Info struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-}
-
-var SecretKey = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
---
+++ example/jwt.go
@@ -0,0 +1,43 @@
+package example
+
+import "github.com/othon-hugo/jwt"
+
+var SecretKey = []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+type Info struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+func Marshal() any {
+	h := jwt.Header{
+		Alg: jwt.HS256,
+		Typ: "JWT",
+	}
+
+	claims := Info{ID: 1, Username: "foobar"}
+
+	token, err := jwt.Marshal(h, claims, SecretKey)
+
+	if err != nil {
+		return err
+	}
+
+	return token
+}
+
+func Unmarshal() any {
+	token, ok := Marshal().(string)
+
+	if !ok {
+		return ok
+	}
+
+	claims := Info{}
+
+	if err := jwt.Unmarshal(token, &claims, SecretKey); err != nil {
+		return err
+	}
+
+	return claims
+}
